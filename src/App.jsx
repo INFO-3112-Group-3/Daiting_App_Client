7dbec48f682d920daec765d2a6d77db48c5cad02
Broden-Pifher <brodenpifher@gmail.com>
2026-03-05 09:26:38 -0500

    Fixed Typo "userState"

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Register from './components/Register'
+import Login from './components/Login'
 function App() {
 
   //state that will store the user that is currently logged in
-  const [user, setUser] = userState();
+  const [user, setUser] = useState();
 
   //function that will be send to the login component to update 
   const updateUser = ((user) =>
