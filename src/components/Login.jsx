0eb04a1a71ec86533d1b28a4613fcfc059de54e3
Broden-Pifher <brodenpifher@gmail.com>
2026-03-05 09:03:15 -0500

    Basic Login Component Test

---
+++ src/components/Login.jsx
@@ -0,0 +1,34 @@
+import {useState} from "react";
+
+import {Paper,TextField, Button} from "@mui/material";
+
+const Login = () => {
+
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const LogIn = (async () => {
+        console.log(userName);
+        console.log(password);
+    })
+    //NOTE: these are stub componets for the desgin, change for actaul development
+    return(
+    <>
+      <TextField fullWidth label="User Name"
+        sx={{mb: "1em"}}
+        value={userName}
+        onChange={setUserName}
+      />
+      <TextField fullWidth label="Password"
+        sx={{mb: "1em"}}
+        value={password}
+        onChange={setPassword}
+      />
+      <Button fullWidth variant="contained" onClick={LogIn}>
+                Log In
+       </Button>
+
+    </>)
+};
+
+export default Login;
